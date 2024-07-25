5ab713695c0f03ca0d0c6f919c9e73a37fea06a9
Danial <bashirzadehdanial@gmail.com>
2024-07-25 23:43:08 +0100

    Add Calculator functionalities

---
+++ Calculator.cs
@@ -0,0 +1,10 @@
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public double Divide(int a, int b) => (b != 0) ? (double)a / b : double.NaN;
+    }
+}
