ff5ef4a12de132bfeb78d06eb23c42e4737ef8b9
Danial <bashirzadehdanial@gmail.com>
2024-07-25 23:46:39 +0100

    Add program class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Utils.LogMessage("Starting Calculator Operations");
+            Console.WriteLine("5 + 3 = " + calc.Add(5, 3));
+            Console.WriteLine("5 - 3 = " + calc.Subtract(5, 3));
+            Console.WriteLine("5 * 3 = " + calc.Multiply(5, 3));
+            Console.WriteLine("5 / 3 = " + calc.Divide(5, 3));
+            Utils.LogMessage("Finished Calculator Operations");
+        }
+    }
+}
